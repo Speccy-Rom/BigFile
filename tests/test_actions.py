86d9847f12f7cc441d6f0285fe645698064d41a0
speccy-rom <speccy.rom@gmail.com>
2021-11-22 23:28:55 +0300

    added: tests for actions and api

---
+++ tests/test_actions.py
@@ -0,0 +1,12 @@
+# Relative Import
+import sys
+
+sys.path.append("..")
+# END Relative Import
+
+from app.actions import get_file_size
+
+
+def test_get_file_size():
+    # assert isinstance(get_file_size('test.txt', '../uploaded_files/'), int) == True
+    assert isinstance(get_file_size('test.txt', './'), int) == True
